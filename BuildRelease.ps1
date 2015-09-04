5d751ab03fdbd1da9929ed4bf5da4405ef2bf492
hoetz <flo@3dshooter.com>
2015-09-04 13:47:52 +0200

    move to powershell build process, improve nuget packaging

---
+++ BuildRelease.ps1
@@ -0,0 +1,36 @@
+function Get-VsVersion ()
+{
+    if ($env:VS140COMNTOOLS -ne $null)
+    {
+        " /property:VisualStudioVersion=14.0"
+    }
+    elseif ($env:VS120COMNTOOLS -ne $null)
+    {
+        " /property:VisualStudioVersion=12.0"
+    }
+}
+
+function Get-BuildConfiguration()
+{
+    $env:config='Release';
+    " /p:Configuration={0}" -f $env:config
+}
+
+function Get-CurrentPackageVersion($pathToAssembly)
+{
+    [System.Diagnostics.FileVersionInfo]$fv=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($pathToAssembly)
+    "{0}.{1}.{2}" -f ($fv.FileMajorPart,$fv.FileMinorPart,$fv.FileBuildPart)
+}
+
+$env:nuget="nuget"
+
+Start-Process -NoNewWindow -Wait -FilePath $env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild -ArgumentList ('src\SimpleAD\SimpleAD.csproj', (Get-VsVersion),(Get-BuildConfiguration))
+
+$env:version=Get-CurrentPackageVersion (Resolve-Path -Path (".\src\SimpleAD\bin\{0}\SimpleAD.dll" -f $env:config) -Relative)
+
+Start-Process -NoNewWindow -Wait -FilePath $env:nuget -ArgumentList ('pack','NugetSpecs\SimpleAD.nuspec', ' -NoPackageAnalysis', ' -verbosity detailed',' -o Build', ('-Version {0}' -f $env:version),' -p',('Configuration={0}' -f $env:config))
+
+
+
+
+
